8c083731fa00216ebbb20d6ebee0dc948cdf50a9
rmccormack <rmccormack@unilink.com>
2026-03-02 16:08:19 +0000

    COSSO-14 : RO & Witness Details

--- src/main/kotlin/uk/gov/justice/digital/hmpps/breachreportcossoapi/controller/ReferenceDataController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/breachreportcossoapi/controller/ReferenceDataController.kt
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -15,6 +16,7 @@ import uk.gov.justice.hmpps.kotlin.common.ErrorResponse
 
 @Validated
 @RestController
+@PreAuthorize("hasRole('ROLE_COSSO')")
 @RequestMapping(value = ["/cosso/referencedata"], produces = ["application/json"])
 class ReferenceDataController(
   private val referenceDataService: ReferenceDataService,
