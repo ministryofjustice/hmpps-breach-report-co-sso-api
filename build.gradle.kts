6b741607d745d8d269ec4a9b7c4c9292152395f6
Paul Miller <paul.miller4@justice.gov.uk>
2026-03-20 14:00:09 +0000

    update jvmTarget => 25 (#41)
    * update jvmTarget => 25
    
    * update jvmTarget => 25

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,11 +30,11 @@ dependencies {
 }
 
 kotlin {
-  jvmToolchain(21)
+  jvmToolchain(25)
 }
 
 tasks {
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    compilerOptions.jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
+    compilerOptions.jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_25
   }
 }
