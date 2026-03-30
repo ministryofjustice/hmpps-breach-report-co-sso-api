afecf7a747dd6dfb6e1c1a8c95125630c1185416
joseph-bcl <41573782+joseph-bcl@users.noreply.github.com>
2026-03-30 11:21:42 +0100

    COSSO-57
    Compliance to date screen changes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/breachreportcossoapi/entity/RequirementEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/breachreportcossoapi/entity/RequirementEntity.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.breachreportcossoapi.entity
 
 import jakarta.persistence.Entity
+import jakarta.persistence.EntityListeners
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
@@ -9,17 +10,19 @@ import org.springframework.data.annotation.CreatedBy
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedBy
 import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 import java.util.UUID
 
 @Entity
+@EntityListeners(AuditingEntityListener::class)
 @Table(name = "cosso_requirement")
 data class RequirementEntity(
   @Id
   val id: UUID = UUID.randomUUID(),
   @ManyToOne
   @JoinColumn(name = "cosso_id")
-  var cosso: CossoEntity,
+  var cosso: CossoEntity? = null,
   var deliusRequirementId: Long,
   var requirementTypeMainCategoryDescription: String? = null,
   var requirementTypeSubCategoryDescription: String? = null,
@@ -33,4 +36,7 @@ data class RequirementEntity(
   var lastUpdatedUser: String? = null,
   @LastModifiedDate
   var lastUpdatedDatetime: LocalDateTime? = null,
+  var notes: String? = null,
+  var failure: Boolean? = null,
+  var failureReason: String? = null,
 )
