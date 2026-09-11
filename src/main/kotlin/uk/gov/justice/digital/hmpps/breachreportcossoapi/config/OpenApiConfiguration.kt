837b8ae0cc62adca84d19760071006d37a3f412e
peter wilson <pwilson@unilink.com>
2026-09-11 15:45:06 +0100

    lint fix

--- src/main/kotlin/uk/gov/justice/digital/hmpps/breachreportcossoapi/config/OpenApiConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/breachreportcossoapi/config/OpenApiConfiguration.kt
@@ -2,9 +2,9 @@ package uk.gov.justice.digital.hmpps.breachreportcossoapi.config
 
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
-import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
 import io.swagger.v3.oas.models.servers.Server
 import org.springframework.boot.info.BuildProperties
